0ae6a5ce2eea850e02d4bc9f433de2ef3e6b17c9
JESUS NAVARRO <JUANJESUS_NAVARRO@Jabil.com>
2023-01-16 18:41:38 -0600

    Data validation

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +21,40 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
-
+app.get('/api/:date?', (req, res) => {
+  if (req.params.date === undefined || req.params.date === '') {
+    console.log("Empty or null");
+    res.json({
+      unix: Date.now(),
+      utc: new Date().toUTCString()
+    });
+  } else {
+    const val = Date.parse(req.params.date);
+    if (!Number.isNaN(val)) {
+      console.log("Normal");
+      res.json({
+        unix: Math.floor(new Date(req.params.date.trim()).getTime()),
+        utc: new Date(req.params.date.trim()).toUTCString()
+      });
+    } else {
+      if (req.params.date.length > 12) {
+        console.log("Parse");
+        var d = new Date(0);
+        d.setUTCMilliseconds(req.params.date);
+        res.json({
+          unix: req.params.date.trim(),
+          utc: d.toUTCString()//To fix
+        });
+      } else {
+        console.log("Error");
+        res.json({ error: "Invalid Date" });
+      }
+    }
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
